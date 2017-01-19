672b3121aed40064a8dd4d654155e31813234e97
Karthik <karthik@karthik.local>
2017-01-19 15:28:14 +0530

    added json write operation

--- server.js
+++ server.js
@@ -1,67 +1,57 @@
 var cric_api_helper = require('./helper.js');
 var jsonfile = require('./playerstats.json');
 var fs = require('fs');
-var request = 35320;
+var request = 3000;
 var obj = {
-   table: []
+    table: []
 };
 
 
 var options = {
     call_type: 'playerStats',
     pid: request
- };
+};
 var enough = true;
 
 
-var players_recursion = function(request){
+var players_recursion = function (request) {
 
-        options.pid= request;
-        cric_api_helper.cricAPICall(options).
+    options.pid = request;
+    cric_api_helper.cricAPICall(options).
         then(function (return_data) {
-            // console.log(return_data);
-            if(return_data.fullName){
+            if (return_data.fullName) {
                 obj.table.push(
                     {
-                        pid: request, 
-                        name:return_data.fullName 
+                        pid: request,
+                        name: return_data.fullName
                     });
             }
-            if(request > 35370){
+            if (request >= 10000) {
+                writefile(obj);
                 enough = false;
             }
             request += 1;
-            if(enough){
-            players_recursion(request);                
+            console.log(request);
+            if (enough) {
+                players_recursion(request);
             }
             console.log(obj);
         }).
         catch(function (err) {
             console.log(err);
         });
-    }
+}
+
+var writefile = function (obj) {
+    var json = JSON.stringify(obj);
+    fs.readFile('playerstats.json', 'utf8', function readFileCallback(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            fs.writeFile('playerstats.json', json, 'utf8'); // write it back
+        }
+    })
+}
+
 
 players_recursion(request);
-
-// try{
-// // for(var i =0; i<3; i++){
-
-// // }
-
-// }
-// catch(err){
-// console.log(err);
-// }
-
-// obj.table.push({id: 1, square:2});
-    // var json = JSON.stringify(obj);
-    // fs.writeFile('myjsonfile.json', json, 'utf8', callback);
-    // fs.readFile('./playerstats.json', 'utf8', function readFileCallback(err, data){
-    //     if (err){
-    //         console.log(err);
-    //     } else {
-    //         obj = JSON.parse(data); //now it an object
-    //         obj.table.push({pid: request, name:""}); //add some data
-    //         json = JSON.stringify(obj); //convert it back to json
-    //         fs.writeFile('myjsonfile.json', json, 'utf8', callback); // write it back
-    //     }})
