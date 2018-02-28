e212321fc5b73ec6c63c4ec2c6a532256b86f89f
Karthik <kgangadhar@corp.homestore.net>
2018-02-28 13:59:33 +0530

    updated script to update players data to mongodb

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
+
 const apecricket = require('ape-cricket');
-const api_key = "yiPB2mqlqdNnPa57Vs8P8S74DXk1"
+const api_key = "q19DZlg6wdgAdlyhYS1Ndh3R0jc2"
 const data = require("./playerstats.json");
 const fs = require("fs");
+const httpRequest = require('request');
+const url = "https://athletes-api.herokuapp.com/crickers";
 var responseArray = [];
+var requestArray = [];
 
 const writefile = (obj) => {
     return new Promise((resolve, reject) => {
-        var json = obj;
-        // var json = JSON.stringify(obj);
-        fs.readFile('playerstat.json', 'utf8', (err, data) => {
-            if (err) {
-                resolve(err);
-            } else {
-                fs.writeFile('playerstat.json', json, 'utf8'); // write it back
-                resolve("OK")
-            }
-        })
+        if (obj && obj.length > 0) {
+            obj.forEach((response) => {
+                validate_response(JSON.parse(response)).then((validResponse) => {
+                    PostResponse(url, validResponse).then((httpResponse) => {
+                        resolve(httpResponse);
+                    }).catch((err) => {
+                        console.log(err);
+                    })
+                })
+            })
+        }
+        resolve(obj);
     });
 }
 
@@ -39,9 +45,61 @@ data.table.forEach(element => {
 })
 
 Promise.all(promiseArray).then(response => {
-    // responseArray.push(JSON.parse(response));
-    writefile(response);  
-}).then(respArr =>{
+    writefile(response);
+}).then(respArr => {
     console.log(respArr);
-    // writefile(JSON.parse(response))
 })
+
+var PostResponse = (url, body) => {
+    return new Promise(function (resolve, reject) {
+        httpRequest.post(url, {
+            json: body,
+        }, (err, resp, body) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(resp.body);
+            }
+        });
+    })
+};
+
+
+const validate_response = (response) => {
+    return new Promise((resolve, reject) => {
+        let data = response
+        if (response && response.data && Object.keys(response.data).length > 0) {
+            format_object(response.data).then((obj) => {
+                resolve(obj);
+            })
+        } else {
+            reject("response ids not valid");
+        }
+    })
+}
+
+const format_object = (playerResponse) => {
+    return new Promise((resolve, reject) => {
+        if (playerResponse) {
+            for (let key in playerResponse) {
+                if (key === "v" || key === "ttl" || key === "provider" || key === "creditsLeft") {
+                    delete playerResponse[key]
+                }
+                else if (key === "pid") {
+                    playerResponse[key] = playerResponse[key].toString();
+                }
+                else if (playerResponse[key] && typeof playerResponse[key] === "object") {
+                    format_object(playerResponse[key])
+                }
+                else if (playerResponse[key] === "" || playerResponse[key] === null || playerResponse[key] === undefined) {
+                    delete playerResponse[key];
+                }
+            }
+            resolve(playerResponse)
+        }
+        else {
+            reject("RESPONSE IS EMPTY");
+        }
+
+    });
+}
